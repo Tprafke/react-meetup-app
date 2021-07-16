f9800cb88d4f249309efc04c6300a79d29ecfda5
toby <rtprafke96@gmail.com>
2021-07-16 14:31:44 -0400

    pagination-and-data-consistency

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import {
-	Button,
-	Container,
-	Header,
-	Icon,
-	Image,
-	Segment,
+  Button,
+  Container,
+  Header,
+  Icon,
+  Segment,
 } from "semantic-ui-react";
 
 export default function HomePage({ history }) {
-	return (
-		<Segment inverted textAlign='center' vertical className='masthead'>
-			<Container>
-				<Header as='h1' inverted>
-					<Image
-						size='massive'
-						src='/assets/logo.png'
-						style={{ marginBottom: 12 }}
-					/>
-					Re-vents
-				</Header>
-				<Button onClick={() => history.push("/events")} size='huge' inverted>
-					Get Started
-					<Icon name='right arrow' inverted />
-				</Button>
-			</Container>
-		</Segment>
-	);
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container>
+        <Header as='h1' inverted>
+          <Icon
+            name='fire'
+            size='large'
+            style={{ marginBottom: 12 }}
+            alt='logo'
+          />
+          EventFire
+        </Header>
+        <Button onClick={() => history.push("/events")} size='huge' inverted>
+          Get Started
+          <Icon name='right arrow' inverted />
+        </Button>
+      </Container>
+    </Segment>
+  );
 }
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { Button, Container, Menu } from "semantic-ui-react";
+import { Button, Container, Icon, Menu } from "semantic-ui-react";
 import SignedInMenu from "./SignedInMenu";
 import SignedOutMenu from "./SignedOutMenu";
 
@@ -12,8 +12,8 @@ export default function NavBar({ setFormOpen }) {
     <Menu inverted fixed='top'>
       <Container>
         <Menu.Item as={NavLink} exact to='/' header>
-          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
-          Re-vents
+          <Icon name='fire' size='large' alt='logo' />
+          EventFire
         </Menu.Item>
         <Menu.Item as={NavLink} to='/events' name='Events' />
         <Menu.Item as={NavLink} to='/sandbox' name='Sandbox' />
